db990b299249315146d3e18319779dc83197c502
wwh <hello@hello.com>
2024-01-28 16:20:59 +0800

    fix

--- src/main/java/com/zzz/pms/sys/Main.java
+++ src/main/java/com/zzz/pms/sys/Main.java
@@ -1,7 +1,6 @@
 package com.zzz.pms.sys;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Main.class);
-        app.setBannerMode(Banner.Mode.OFF);
+//        app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
     }
 
